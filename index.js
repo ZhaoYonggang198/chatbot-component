545b7700ef9106aedceed5c19de709b9ff5eda41
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-08-30 10:57:24 +0800

    第一个版本

---
+++ index.js
@@ -0,0 +1,9 @@
+import { setChatbotUrl, setAsrUrl, setImageUrl } from './api/url.js'
+import { setUserid } from './api/userid.js'
+
+module.exports = {
+  setChatbotUrl,
+  setAsrUrl,
+  setImageUrl,
+  setUserid
+}
